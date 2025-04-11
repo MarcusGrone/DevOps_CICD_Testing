1d1c40e0abcaba139e4216e12c708aa25fc6bfcd
Marcus Grøne <marcusgrone@gmail.com>
2025-04-11 08:11:04 +0200

    initial ci-push

---
+++ src/main/java/SWE/App.java
@@ -0,0 +1,11 @@
+package SWE;
+
+public class App  {
+  public static String sayHello() {
+    return "Hello, CI/CD!";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sayHello());
+  }
+}
---
+++ src/test/java/SWE/AppTest.java
@@ -0,0 +1,12 @@
+package SWE;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+  @Test
+  public void testSayHello() {
+    assertEquals("Hello, CI/CD!", App.sayHello());
+  }
+}
